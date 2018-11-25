dca2da906fddbb7b9a736216838d859e3f91204c
yogeshwaran <yogesh.ktm@gmail.com>
2018-11-25 20:51:27 +0530

    Updates

---
+++ precache-manifest.129e49d944dd2dd7c26cc435b6439bc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a705cdf5d286614bb30f",
+    "url": "/react-quote-app/static/js/runtime~main.a705cdf5.js"
+  },
+  {
+    "revision": "e7dc9a750bdf0ac240d3",
+    "url": "/react-quote-app/static/js/main.e7dc9a75.chunk.js"
+  },
+  {
+    "revision": "9b9b3b5b332384ae9cea",
+    "url": "/react-quote-app/static/js/1.9b9b3b5b.chunk.js"
+  },
+  {
+    "revision": "e7dc9a750bdf0ac240d3",
+    "url": "/react-quote-app/static/css/main.09d5a292.chunk.css"
+  },
+  {
+    "revision": "115eb97d48419103c9963335fb0645cf",
+    "url": "/react-quote-app/index.html"
+  }
+];
