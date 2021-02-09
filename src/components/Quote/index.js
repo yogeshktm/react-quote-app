e03287df56d5734355f503e9a1ea1fb5ef560a87
yogesh.ktm <yogesh.ktm@gmail.com>
2021-02-09 17:06:23 +0530

    Get another quote button added

--- src/components/Quote/index.js
+++ src/components/Quote/index.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React from "react";
 
-export default class Quote extends React.Component{
-  render(){
+export default class Quote extends React.Component {
+  render() {
     let loading = this.props.loading;
-    return(
+    return (
       <section className="quote-wrapper">
-        <h2 className="quote-title">{this.props.category} Quote of the day</h2>
+        <h2 className="quote-title">React random quotes</h2>
         <blockquote>
-        <div class={loading ? "loading" : "loaded"}>
-          <div class="loader-line"></div>
-          <div class="loader-line loader-line-2"></div>
-          <div class="loader-line loader-line-3"></div>
-          <div class="loader-line loader-line-4"></div>
-        </div>
-        {this.props.quote}
+          {this.props.loading ? (
+            <div class={loading ? "loading" : "loaded"}>
+              <div class="loader-line" />
+              <div class="loader-line loader-line-2" />
+              <div class="loader-line loader-line-3" />
+              <div class="loader-line loader-line-4" />
+            </div>
+          ) : (
+            this.props.quote
+          )}
         </blockquote>
         <p className="author">
-        <div class={loading ? "loading" : "loaded"}>
-          <div class="loader-line loader-line-4"></div>
-        </div>
-        {this.props.loading ? "" : "-"} {this.props.author}</p>
+          {this.props.loading ? (
+            <div class={loading ? "loading" : "loaded"}>
+              <div class="loader-line loader-line-4" />
+            </div>
+          ) : (
+            this.props.author
+          )}
+        </p>
+        <button class="btn" onClick={this.props.handleClick}>
+          Give me another random quote
+        </button>
       </section>
-    )
+    );
   }
 }
--- src/containers/QuoteContainer.js
+++ src/containers/QuoteContainer.js
@@ -1,41 +1,49 @@
-import React from 'react';
-import axios from 'axios';
-import Quote from '../components/Quote/';
+import React from "react";
+import axios from "axios";
+import Quote from "../components/Quote/";
 
-
-
-export class QuoteContainer extends React.Component{
-  constructor(props){
+export class QuoteContainer extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      author:"",
-      quote:"",
-      category:"",
-      loading:true
+      author: "",
+      quote: "",
+      category: "",
+      loading: true,
     };
+    this.getQuote = this.getQuote.bind(this);
   }
-  componentwillMount(){
-
+  componentwillMount() {}
+  componentDidMount() {
+    this.getQuote();
   }
-  componentDidMount(){
+  getQuote() {
     const url = "https://api.quotable.io/random";
-    axios
-     .get(url)
-     .then(response => {
-       let quoteResponse = response.data;
-       console.log(quoteResponse);
-       this.setState({
-         loading:false,
-         author:quoteResponse.author,
-         quote:quoteResponse.content,
-         category:quoteResponse.tags[0],
-       })
-     })
-     .catch(error => console.log(error));
+    this.setState({ loading: true }, () => {
+      axios
+        .get(url)
+        .then((response) => {
+          let quoteResponse = response.data;
+          console.log(quoteResponse);
+          this.setState({
+            loading: false,
+            author: quoteResponse.author,
+            quote: quoteResponse.content,
+            category: quoteResponse.tags[0],
+          });
+        })
+        .catch((error) => console.log(error));
+    });
   }
-  render(){
-      return(
-        <Quote loading={this.state.loading} author={this.state.author} quote={this.state.quote} category={this.state.category}/>
-      );
-    }
+  render() {
+    return (
+      <Quote
+        loading={this.state.loading}
+        author={this.state.author}
+        quote={this.state.quote}
+        category={this.state.category}
+        handleClick={this.getQuote}
+      />
+    );
+  }
 }
